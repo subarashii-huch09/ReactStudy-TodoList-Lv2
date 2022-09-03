c221291de5bfe1c7e6a860283063ac2ba88ed040
Ching-wen Hu <ching-wen.hu@developersinstitute.co.nz>
2022-09-03 14:33:17 +1200

    add Routes,Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import styles  from "./App.module.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login"
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className={styles.App}>
+      <Routes>
+        <Route path="/" element={<Login/>}></Route>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
     </div>
   );
 }
