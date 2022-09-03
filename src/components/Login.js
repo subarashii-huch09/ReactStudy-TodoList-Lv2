eb01188af2a0afb03ae4ad456e64acde8291d0cb
Ching-wen Hu <ching-wen.hu@developersinstitute.co.nz>
2022-09-03 16:33:25 +1200

    add classes

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import {Link} from "react-router-dom"
 import Side from "./Side";
-import styles from "./Login.module.css";
+import styles from "./LoginSignUp.module.css";
 
 const Login = () => {
   const {
@@ -16,28 +16,26 @@ const Login = () => {
     <div className={styles.container}>
       <Side />
       <div>
-        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-          <h2 className={styles.loginTitle}>最實用的線上代辦事項服務</h2>
+        <form className={`${styles.form} ${styles.loginForm}`} onSubmit={handleSubmit(onSubmit)}>
+          <h2 className={styles.formTitle}>最實用的線上代辦事項服務</h2>
           <div className={styles.formControl}>
-            <label className={styles.formControlLabel} for="email">
+            <label className={styles.formControlLabel} htmlFor="email">
               Email
             </label>
             <input
               className={styles.formControlInput}
               id="email"
               name="email"
-              type="text"
               placeholder="請輸入Email"
               {...register("Email", {
-                required: true,
-                pattern: /^\S+@\S+$/i,
-                message: "此欄位不可留空",
+                required: { value: true, message: "此欄位必填寫" },
+                pattern: { value: /^\S+@\S+$/i, message: "不符合 Email 規則" },
               })}
             />
             <span>{errors.email?.message}</span>
           </div>
           <div className={styles.formControl}>
-            <label className={styles.formControlLabel} for="password">
+            <label className={styles.formControlLabel} htmlFor="password">
               密碼
             </label>
             <input
@@ -46,20 +44,19 @@ const Login = () => {
               id="password"
               type="password"
               placeholder="請輸入密碼"
-              {...register("密碼", {
-                required: true,
-                maxLength: 80,
-                message: "此欄位不可留空",
+              {...register("password", {
+                required: { value: true, message: "此欄位必填寫" },
+                minLength: { value: 8, message: "密碼至少為 8 碼" },
               })}
             />
             <span>{errors.password?.message}</span>
           </div>
           <input
-            className={styles.formControlSubmit}
+            className={styles.formControlSubmitBtn}
             type="submit"
-            value="Log in"
+            value="登入"
           />
-          <Link className={styles.formControl_btnLink} to="/signUp">
+          <Link className={styles.formControlBtnLink} to="/signUp">
             註冊帳號
           </Link>
         </form>
