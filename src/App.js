a3968fe7aaf2e8db56d72b1ed1511b755d7eb48b
Ching-wen Hu <ching-wen.hu@developersinstitute.co.nz>
2022-09-03 20:08:40 +1200

    add TodoList Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import styles  from "./App.module.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login"
+import SignUp from "./components/SignUp"
+import TodoList from "./components/TodoList"
 
 function App() {
   return (
     <div className={styles.App}>
       <Routes>
         <Route path="/" element={<Login/>}></Route>
-        <Route></Route>
-        <Route></Route>
+        <Route path="/signUp" element={<SignUp/>}></Route>
+        <Route path="/todos" element={<TodoList/>}></Route>
       </Routes>
     </div>
   );
