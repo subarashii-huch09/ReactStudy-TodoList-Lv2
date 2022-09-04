8751d028c79096d02ea764a1bd6b2b748e35942b
Ching-wen Hu <ching-wen.hu@developersinstitute.co.nz>
2022-09-04 17:07:08 +1200

    add route

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login"
 import SignUp from "./components/SignUp"
 import TodoList from "./components/TodoList"
+import NotFoundPage from "./components/NotFoundPage";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Routes>
-        <Route path="/" element={<Login/>}></Route>
-        <Route path="/signUp" element={<SignUp/>}></Route>
-        <Route path="/todos" element={<TodoList/>}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/signUp" element={<SignUp />}></Route>
+          <Route path="/todos" element={<TodoList />}></Route>
+          <Route path="*" element={<NotFoundPage />}></Route>
+        </Routes>
     </div>
   );
 }
